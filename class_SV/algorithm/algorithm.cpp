e7c494817fa0a9c4e36881dceca20058b40d0314
Teobka99 <anhtaibka99@gmail.com>
2024-08-08 03:32:56 +0700

    add algorithm

---
+++ class_SV/algorithm/algorithm.cpp
@@ -0,0 +1,70 @@
+#include "algorithm_test.h"
+#include<istream>
+#include<iomanip>
+#include<string>
+#include<array>
+#include<vector>
+#include<iomanip>
+#include<math.h>
+
+using namespace std;
+
+void sortarr::swap(int& a, int& b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+void sortarr::show_arr(vector<int>& arr)
+{
+	cout << "arr: ";
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+// interchange sort
+void sortarr::interchange_sort(vector<int>& arr)
+{
+	int length = arr.size();
+	for (int i = 0; i < length - 1; i++)
+	{
+		for (int j = i +1; j < length; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				sortarr::swap(arr[i], arr[j]);
+			}
+		}
+	}
+}
+
+
+// budble sort
+void sortarr::buble_sort(vector<int>& arr)
+{
+	int length = arr.size();
+	for (int i = 1; i < length; i++)
+	{
+		for (int j = i -1; j < length -1; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				sortarr::swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
+
+int main()
+{
+	sortarr s1;
+	vector<int> arr = { 1,5,7,9,24,59,24,148,15156,1416,6214242,1414 };
+	s1.buble_sort(arr);
+	s1.show_arr(arr);
+}
---
+++ class_SV/algorithm/algorithm_test.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<array>
+#include<vector>
+#include<iomanip>
+#include<random>
+
+using namespace std;
+
+class sortarr
+{
+public:
+	void swap(int& a, int& b);
+	void interchange_sort(vector<int>& arr);
+	void buble_sort(vector<int>& arr);
+	void insersort(vector<int>& arr);
+	void selection_sort(vector<int>& arr);
+	void quick_sort(vector<int>& arr);
+	void show_arr(vector<int> &arr);
+};
+
